05b57ad1e860b846d03cb619da46b0734107b63d
Marcin Filipowski <proartz.devon@gmail.com>
2019-04-18 15:57:10 +0200

    Add exercise 05 solution

---
+++ src/main/java/com/proartz/Ch01E05/App.java
@@ -0,0 +1,15 @@
+package com.proartz.Ch01E05;
+
+public class App {
+
+    public static void main(String[] args) {
+        double number = Integer.MAX_VALUE + 500.0;
+//        number++;
+        int castedNumber = (int)number;
+
+
+        System.out.printf("Integer.MAX_VALUE: %d%n", Integer.MAX_VALUE);
+        System.out.printf("Number: %f%n", number);
+        System.out.printf("CastedNumber: %d%n", castedNumber);
+    }
+}
