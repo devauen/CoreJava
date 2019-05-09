81e830037db74dcba819bf238969e47aa726d924
Marcin Filipowski <proartz.devon@gmail.com>
2019-05-09 16:49:20 +0200

    Add Chapter 4 Exercise 9 solution

---
+++ src/main/java/com/proartz/Chapter4/Exercise9/test.java
@@ -0,0 +1,41 @@
+package com.proartz.Chapter4.Exercise9;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+public class test {
+
+    private int number = 0;
+    public static String name = "blabla";
+
+    @Override
+    public String toString(){
+        StringBuilder builder = new StringBuilder();
+
+        try {
+
+            // add a package name
+            builder.append("Package: " + getClass().getPackage().getName() + "\n");
+
+            // add a class name
+            builder.append(Modifier.toString(getClass().getModifiers()) + " ");
+            builder.append(getClass().getSimpleName() + " {\n");
+
+            // add all fields info
+            for (Field f : getClass().getDeclaredFields()) {
+                f.setAccessible(true);
+                builder.append("\t" + Modifier.toString(f.getModifiers()) + " " +
+                        f.getType().getSimpleName() + " " +
+                        f.getName() + " = " +
+                        f.get(this) + ";\n");
+            }
+
+            builder.append("}\n");
+
+        } catch (IllegalAccessException ex) {
+            ex.printStackTrace();
+        }
+
+        return builder.toString();
+    }
+}
