39afc3f2cc4222fb40a6c99b7a3a97d6921e7b40
Marcin Filipowski <proartz.devon@gmail.com>
2019-05-15 17:55:13 +0200

    Add Chapter 6 Exercise 5 solution

---
+++ src/main/java/com/proartz/Chapter6/Exercise5/Swap.java
@@ -0,0 +1,22 @@
+package com.proartz.Chapter6.Exercise5;
+
+public class Swap {
+
+    /*
+    * Dodanie jawnej informacji o typie przed wywołaniem metody swap, znacząco poprawia
+    * czytelność błędów. 
+    * */
+    public static void main(String[] args) {
+
+        Double[] result = Swap.<Double>swap(0, 1, 1.5, 2.0, 3.0);
+
+    }
+
+    public static <T> T[] swap(int i, int j, T... values) {
+        T temp = values[i];
+        values[i] = values[j];
+        values[j] = temp;
+        return values;
+    }
+
+}
